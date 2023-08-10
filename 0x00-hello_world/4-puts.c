311a5fdcf225047179b50105753cf68dd0922128
Nourhan-Hossam <Nourhosam1111@gmail.com>
2023-08-10 14:18:18 -0700

    gg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main -Entry point
  *
  * Description: A C program that prints with put function
  *
- * Return: Alawys 0 (success)
+ * Return: Alawys (0)
 */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);	
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
