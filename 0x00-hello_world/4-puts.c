9b3c6e77da5a5b5de5fa9ae46b52a31d78425a71
Nourhan-Hossam <Nourhosam1111@gmail.com>
2023-08-10 13:51:51 -0700

    jj

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,9 @@
  *
  * Return: Alawys 0 (success)
 */
+
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);	
 }
